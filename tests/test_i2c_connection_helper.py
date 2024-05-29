19be83c0e85b0fa64c4c674c12f9c472f89c266a
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2024-05-29 15:13:21 +0100

    Added tests for the generic read/write functions

--- tests/test_i2c_connection_helper.py
+++ tests/test_i2c_connection_helper.py
@@ -22,9 +22,15 @@
 #############################################################################
 
 import logging
+import random
+from math import ceil
+from unittest.mock import call
+from unittest.mock import patch
 
 import pytest
 
+from i2c_gui2.functions import bytes_to_word_list
+from i2c_gui2.functions import word_list_to_bytes
 from i2c_gui2.i2c_connection_helper import I2C_Connection_Helper
 from i2c_gui2.i2c_messages import I2CMessages
 
@@ -174,3 +180,326 @@ def test_check_i2c_device(caplog, connect_return_value, fake_connect, i2c_ch_no_
     assert log_tuples[0][1] == logging.INFO
     assert log_tuples[1][1] == logging.INFO
     # assert log_tuples[0][2] == "Test Debug"
+
+
+def test_read_device_memory_not_connected(i2c_ch_test):
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.read_device_memory(0x21, 0x00, 1)
+    assert e_info.match(r"^You must first connect to a device before trying to read registers from it")
+
+
+def test_write_device_memory_not_connected(i2c_ch_test):
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.write_device_memory(0x21, 0x00, [0x54])
+    assert e_info.match(r"^You must first connect to a device before trying to write registers to it")
+
+
+def test_read_device_memory_invalid_device_address(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.read_device_memory(0x80, 0x00, 1)
+    assert e_info.match(r"^Invalid I2C address received: 0x80")
+
+
+def test_write_device_memory_invalid_device_address(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.write_device_memory(0x80, 0x00, [0x54])
+    assert e_info.match(r"^Invalid I2C address received: 0x80")
+
+
+def test_read_device_memory_invalid_address_endianness(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.read_device_memory(0x21, 0x00, 1, address_endianness='blabla')
+    assert e_info.match(r"^A wrong address endianness was set: blabla")
+
+
+def test_write_device_memory_invalid_address_endianness(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.write_device_memory(0x21, 0x00, [0x54], address_endianness='blabla')
+    assert e_info.match(r"^A wrong address endianness was set: blabla")
+
+
+def test_read_device_memory_invalid_word_endianness(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.read_device_memory(0x21, 0x00, 1, word_endianness='blabla')
+    assert e_info.match(r"^A wrong word endianness was set: blabla")
+
+
+def test_write_device_memory_invalid_word_endianness(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.write_device_memory(0x21, 0x00, [0x54], word_endianness='blabla')
+    assert e_info.match(r"^A wrong word endianness was set: blabla")
+
+
+def test_read_device_memory_invalid_read_type(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.read_device_memory(0x21, 0x00, 1, read_type='blabla')
+    assert e_info.match(r"^A wrong read type was set: blabla")
+
+
+def test_write_device_memory_invalid_read_type(i2c_ch_test):
+    i2c_ch_test._is_connected = True
+    with pytest.raises(Exception) as e_info:
+        i2c_ch_test.write_device_memory(0x21, 0x00, 1, write_type='blabla')
+    assert e_info.match(r"^A wrong write type was set: blabla")
+
+
+@pytest.mark.parametrize('words', [1, 2, 4])
+@pytest.mark.parametrize('bitlength', [8, 16])
+@pytest.mark.parametrize('endianness', ['big', 'little'])
+def test_read_device_memory_no_connect(caplog, i2c_ch_test, words, bitlength, endianness):
+    caplog.set_level(logging.DEBUG, "I2C_Log")
+    i2c_ch_test._is_connected = True
+
+    word_list = i2c_ch_test.read_device_memory(0x21, 0x00, words, word_bitlength=bitlength, word_endianness=endianness)
+
+    log_tuples = caplog.record_tuples
+    assert len(log_tuples) == 2
+    assert log_tuples[0][0] == "I2C_Log"
+    assert log_tuples[1][0] == "I2C_Log"
+    assert log_tuples[0][1] == logging.INFO
+    assert log_tuples[1][1] == logging.DEBUG
+    assert "Software emulation (no connect) is enabled, so returning dummy values:" in log_tuples[1][2]
+
+    assert len(word_list) == words
+    if words == 1:
+        assert "Reading the register" in log_tuples[0][2]
+        assert word_list[0] == 42
+    else:
+        assert "Reading a register block with size" in log_tuples[0][2]
+        if bitlength == 8:
+            for i in range(words):
+                assert word_list[i] == i
+        else:
+            byte_list = [i for i in range(words * ceil(bitlength / 8))]
+            compare_list = bytes_to_word_list(byte_list, ceil(bitlength / 8), endianness)
+            assert word_list == compare_list
+
+
+@pytest.mark.parametrize('words', [1, 2, 4])
+@pytest.mark.parametrize('bitlength', [8, 16])
+@pytest.mark.parametrize('endianness', ['big', 'little'])
+def test_write_device_memory_no_connect(caplog, i2c_ch_test, words, bitlength, endianness):
+    caplog.set_level(logging.DEBUG, "I2C_Log")
+    i2c_ch_test._is_connected = True
+
+    word_list = [i for i in range(words)]
+
+    i2c_ch_test.write_device_memory(0x21, 0x00, word_list, word_bitlength=bitlength, word_endianness=endianness)
+
+    log_tuples = caplog.record_tuples
+    assert len(log_tuples) == 2
+    assert log_tuples[0][0] == "I2C_Log"
+    assert log_tuples[1][0] == "I2C_Log"
+    assert log_tuples[0][1] == logging.INFO
+    assert log_tuples[1][1] == logging.DEBUG
+    assert log_tuples[1][2] == "Software emulation (no connect) is enabled, so no write action is taken."
+
+    assert len(word_list) == words
+    if words == 1:
+        assert "Writing the value" in log_tuples[0][2]
+    else:
+        assert "Writing a register block with size" in log_tuples[0][2]
+
+
+@pytest.mark.parametrize('i2c_ch_max_seq_byte', [None])
+@pytest.mark.parametrize('i2c_ch_no_connect', [False])
+@pytest.mark.parametrize('words', [1, 2])
+@pytest.mark.parametrize('bitlength', [8, 16])
+@pytest.mark.parametrize('endianness', ['big', 'little'])
+def test_read_device_memory_no_max_seq_byte(caplog, i2c_ch_test, words, bitlength, endianness):
+    caplog.set_level(logging.DEBUG, "I2C_Log")
+    i2c_ch_test._is_connected = True
+
+    with patch('i2c_gui2.i2c_connection_helper.I2C_Connection_Helper._read_i2c_device_memory') as function:
+        ret_list = [random.getrandbits(8) for _ in range(words * ceil(bitlength / 8))]
+        function.return_value = ret_list
+
+        word_list = i2c_ch_test.read_device_memory(0x21, 0x00, words, word_bitlength=bitlength, word_endianness=endianness)
+
+        function.assert_called_once()
+        function.assert_has_calls([call(0x21, 0x00, words * ceil(bitlength / 8), read_type="Normal")])
+
+        log_tuples = caplog.record_tuples
+        assert len(log_tuples) == 2
+        assert log_tuples[0][0] == "I2C_Log"
+        assert log_tuples[1][0] == "I2C_Log"
+        assert log_tuples[0][1] == logging.INFO
+        assert log_tuples[1][1] == logging.DEBUG
+        assert "Got data:" in log_tuples[1][2]
+
+        assert len(word_list) == words
+
+        compare_list = bytes_to_word_list(ret_list, ceil(bitlength / 8), endianness)
+        assert word_list == compare_list
+
+        if words == 1:
+            assert "Reading the register" in log_tuples[0][2]
+        else:
+            assert "Reading a register block with size" in log_tuples[0][2]
+
+
+@pytest.mark.parametrize('i2c_ch_max_seq_byte', [None])
+@pytest.mark.parametrize('i2c_ch_no_connect', [False])
+@pytest.mark.parametrize('words', [1, 2])
+@pytest.mark.parametrize('bitlength', [8, 16])
+@pytest.mark.parametrize('endianness', ['big', 'little'])
+def test_write_device_memory_no_max_seq_byte(caplog, i2c_ch_test, words, bitlength, endianness):
+    caplog.set_level(logging.DEBUG, "I2C_Log")
+    i2c_ch_test._is_connected = True
+
+    word_list = [random.getrandbits(bitlength) for _ in range(words)]
+    byte_list = word_list_to_bytes(word_list, ceil(bitlength / 8), endianness)
+
+    with patch('i2c_gui2.i2c_connection_helper.I2C_Connection_Helper._write_i2c_device_memory') as function:
+        i2c_ch_test.write_device_memory(0x21, 0x00, word_list, word_bitlength=bitlength, word_endianness=endianness)
+
+        function.assert_called_once()
+        function.assert_has_calls([call(0x21, 0x00, byte_list, write_type="Normal")])
+
+        log_tuples = caplog.record_tuples
+        assert len(log_tuples) == 2
+        assert log_tuples[0][0] == "I2C_Log"
+        assert log_tuples[1][0] == "I2C_Log"
+        assert log_tuples[0][1] == logging.INFO
+        assert log_tuples[1][1] == logging.DEBUG
+        assert log_tuples[1][2] == "Writing the full block at once."
+
+        if words == 1:
+            assert "Writing the value" in log_tuples[0][2]
+        else:
+            assert "Writing a register block with size" in log_tuples[0][2]
+
+
+@pytest.mark.parametrize('i2c_ch_max_seq_byte', [1, 2, 8])
+@pytest.mark.parametrize('i2c_ch_no_connect', [False])
+@pytest.mark.parametrize('words', [1, 2, 8])
+@pytest.mark.parametrize('bitlength', [8, 16])
+@pytest.mark.parametrize('endianness', ['big', 'little'])
+def test_read_device_memory(caplog, i2c_ch_test, words, bitlength, endianness, i2c_ch_max_seq_byte):
+    caplog.set_level(logging.DEBUG, "I2C_Log")
+    i2c_ch_test._is_connected = True
+
+    with patch('i2c_gui2.i2c_connection_helper.I2C_Connection_Helper._read_i2c_device_memory') as function:
+        ret_list = [random.getrandbits(8) for _ in range(min(words * ceil(bitlength / 8), i2c_ch_max_seq_byte))]
+        function.return_value = ret_list
+
+        if ceil(bitlength / 8) > i2c_ch_max_seq_byte:
+            with pytest.raises(Exception) as e_info:
+                word_list = i2c_ch_test.read_device_memory(0x21, 0x00, words, word_bitlength=bitlength, word_endianness=endianness)
+            assert e_info.match(r"^The word length is too big for the maximum number of bytes")
+
+            function.assert_not_called()
+        else:
+            word_list = i2c_ch_test.read_device_memory(0x21, 0x00, words, word_bitlength=bitlength, word_endianness=endianness)
+
+            num_calls = ceil(words * ceil(bitlength / 8) / i2c_ch_max_seq_byte)
+            addr_step = i2c_ch_max_seq_byte / (bitlength / 8)
+
+            function.assert_has_calls(
+                [
+                    call(0x21, int(i * addr_step), min(words * ceil(bitlength / 8), i2c_ch_max_seq_byte), read_type="Normal")
+                    for i in range(num_calls)
+                ]
+            )
+
+            log_tuples = caplog.record_tuples
+            assert len(log_tuples) == 2 * num_calls + 2
+            for i in range(2 * num_calls + 2):
+                assert log_tuples[i][0] == "I2C_Log"
+                if i == 0:
+                    assert log_tuples[i][1] == logging.INFO
+                else:
+                    assert log_tuples[i][1] == logging.DEBUG
+                if i == 0:
+                    continue
+                elif i == 1:
+                    assert f"Breaking the read into {num_calls}" in log_tuples[i][2]
+                else:
+                    if i % 2 == 0:
+                        assert f"Read operation {int(i/2 - 1)}" in log_tuples[i][2]
+                    else:
+                        assert "Got data:" in log_tuples[i][2]
+
+            assert len(word_list) == words
+
+            compare_list = []
+            for i in range(num_calls):
+                compare_list += ret_list
+            compare_list = bytes_to_word_list(compare_list, ceil(bitlength / 8), endianness)
+            assert word_list == compare_list
+
+            if words == 1:
+                assert "Reading the register" in log_tuples[0][2]
+            else:
+                assert "Reading a register block with size" in log_tuples[0][2]
+
+
+@pytest.mark.parametrize('i2c_ch_max_seq_byte', [1, 2, 8])
+@pytest.mark.parametrize('i2c_ch_no_connect', [False])
+@pytest.mark.parametrize('words', [1, 2, 8])
+@pytest.mark.parametrize('bitlength', [8, 16])
+@pytest.mark.parametrize('endianness', ['big', 'little'])
+def test_write_device_memory(caplog, i2c_ch_test, words, bitlength, endianness, i2c_ch_max_seq_byte):
+    caplog.set_level(logging.DEBUG, "I2C_Log")
+    i2c_ch_test._is_connected = True
+
+    word_list = [random.getrandbits(bitlength) for _ in range(words)]
+    byte_list = word_list_to_bytes(word_list, ceil(bitlength / 8), endianness)
+
+    with patch('i2c_gui2.i2c_connection_helper.I2C_Connection_Helper._write_i2c_device_memory') as function:
+
+        if ceil(bitlength / 8) > i2c_ch_max_seq_byte:
+            with pytest.raises(Exception) as e_info:
+                i2c_ch_test.write_device_memory(0x21, 0x00, word_list, word_bitlength=bitlength, word_endianness=endianness)
+            assert e_info.match(r"^The word length is too big for the maximum number of bytes")
+
+            function.assert_not_called()
+        else:
+            i2c_ch_test.write_device_memory(0x21, 0x00, word_list, word_bitlength=bitlength, word_endianness=endianness)
+
+            num_calls = ceil(words * ceil(bitlength / 8) / i2c_ch_max_seq_byte)
+            addr_step = i2c_ch_max_seq_byte / (bitlength / 8)
+
+            function.assert_has_calls(
+                [
+                    call(
+                        0x21,
+                        int(i * addr_step),
+                        byte_list[
+                            i * i2c_ch_max_seq_byte : i * i2c_ch_max_seq_byte + min(words * ceil(bitlength / 8), i2c_ch_max_seq_byte)
+                        ],
+                        write_type="Normal",
+                    )
+                    for i in range(num_calls)
+                ]
+            )
+
+            log_tuples = caplog.record_tuples
+            assert len(log_tuples) == 2 * num_calls + 2
+            for i in range(2 * num_calls + 2):
+                assert log_tuples[i][0] == "I2C_Log"
+                if i == 0:
+                    assert log_tuples[i][1] == logging.INFO
+                else:
+                    assert log_tuples[i][1] == logging.DEBUG
+                if i == 0:
+                    continue
+                elif i == 1:
+                    assert f"Breaking the write into {num_calls}" in log_tuples[i][2]
+                else:
+                    if i % 2 == 0:
+                        assert f"Write operation {int(i/2 - 1)}" in log_tuples[i][2]
+                    else:
+                        assert "Current block:" in log_tuples[i][2]
+
+            if words == 1:
+                assert "Writing the value" in log_tuples[0][2]
+            else:
+                assert "Writing a register block with size" in log_tuples[0][2]
